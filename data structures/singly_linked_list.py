4df48bfa3974159f4ad43bdacd6306c037c10c7a
horpeazy <iyamuhope.nosa647@gmail.com>
2022-10-04 03:52:12 +0200

    feat: sort linked list

--- data structures/singly_linked_list.py
+++ data structures/singly_linked_list.py
@@ -248,7 +248,7 @@ def merge(list1, list2):
 class SortedLinkedList:
     def __init__(self):
         self.head = None
-    
+
     def append(self, value):
         """
         Append a value to the Linked List in ascending sorted order
@@ -259,19 +259,42 @@ class SortedLinkedList:
         if self.head is None:
             self.head = Node(value)
             return
-        
+
         if value < self.head.value:
             node = Node(value)
             node.next = self.head
             self.head = node
             return
-        
+
         node = self.head
         while node.next is not None and value >= node.next.value:
             node = node.next
-            
+
         new_node = Node(value)
         new_node.next = node.next
         node.next = new_node
-      
+
         return None
+
+def sort(array):
+    """
+    Given an array of integers, use SortedLinkedList to sort them and return a sorted array.
+
+    Args:
+       array(array): Array of integers to be sorted
+    Returns:
+       array: Return sorted array
+    """
+    sorted_array = []
+    
+    linked_list = SortedLinkedList()
+    for value in array:
+        linked_list.append(value)
+    
+    # Convert sorted linked list to a normal list/array
+    node = linked_list.head
+    while node:
+        sorted_array.append(node.value)
+        node = node.next
+    
+    return sorted_array
