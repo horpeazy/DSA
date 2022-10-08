7c7925fbe4b1771e52a8975b3dcd985dded16d34
horpeazy <iyamuhope.nosa647@gmail.com>
2022-10-08 15:21:37 +0200

    feat: pascals triangle

--- data structures/singly_linked_list.py
+++ data structures/singly_linked_list.py
@@ -318,3 +318,99 @@ def add_one(arr):
         number = math.floor(number / 10)
         
     return new_arr
+
+# ------------------------------------------------ #
+#   Max Sub Array                                  #
+# ------------------------------------------------ #
+
+# Solution
+'''
+The Idea:
+1. We have to find the sum of "contiguous" subarray, therefore we must not change the order of array elements.
+2. Let `current_sum` denotes the sum of a subarray, and `max_sum` denotes the maximum value of `current_sum`.
+3. LOOP STARTS: For each element of the array, update the `current_sum` with the MAXIMUM of either:
+ - The element added to the `current_sum` (denotes the addition of the element to the current subarray)
+ - The element itself  (denotes the starting of a new subarray)
+ - Update (overwrite) `max_sum`, if it is lower than the updated `current_sum`
+4. Return `max_sum`
+'''
+
+def max_sum_subarray(arr):
+    
+    current_sum = arr[0] # `current_sum` denotes the sum of a subarray
+    max_sum = arr[0]     # `max_sum` denotes the maximum value of `current_sum` ever
+    
+    # Loop from VALUE at index position 1 till the end of the array
+    for element in arr[1:]:
+        
+        '''
+        # Compare (current_sum + element) vs (element)
+        # If (current_sum + element) is higher, it denotes the addition of the element to the current subarray
+        # If (element) alone is higher, it denotes the starting of a new subarray
+        '''
+        current_sum = max(current_sum + element, element)
+        
+        # Update (overwrite) `max_sum`, if it is lower than the updated `current_sum`
+        max_sum = max(current_sum, max_sum)   
+    
+    return max_sum
+
+def max_sum_subarray(arr):
+    """
+    :param - arr - input array
+    return - number - largest sum in contiguous subarry within arr
+    """
+    sums = []
+    
+    for i in range(len(arr)):
+        sum = arr[i]
+        for j in range(i + 1, len(arr)):
+            sum += arr[j]
+            sums.append(sum)
+    return max(sums)
+
+
+# ------------------------------------------------ #
+#   Pascals Triangle                               #
+# ------------------------------------------------ #
+
+# Solution
+
+'''
+Points to note:
+1. We have to return a list.
+2. The elements of n^th row are made up of elements of (n-1)^th row. This comes up till the 1^st row. We will follow a top-down approach. 
+3. Except for the first and last element, any other element at position `j` in the current row is the sum of elements at position `j` and `j-1` in the previous row. 
+4. Be careful about the edge cases, example, an index should never be a NEGATIVE at any point of time. 
+'''
+
+def nth_row_pascal(n):
+    
+    if n == 0:
+        return [1]
+    
+    current_row = [1] # First row
+    
+    ''' Loop from 1 to n; `i` denotes the row number'''
+    for i in range(1, n + 1):
+        # Set the `current_row` from previous iteration as the `previous_row`
+        previous_row = current_row
+        
+        # Let's build the fresh current_row gradually
+        current_row = [1] # add the default first element at the 0^th index of the `i^th` row
+        
+        '''Loop from 1 to (i-1); `j` denotes the index of an element with in the `i^th` row'''
+        # Example, for 5th row we have considered n=4, 
+        # we will iterate index from 1 to 3, because 
+        # the default element at the 0^th index has already been added
+        for j in range(1, i):
+            
+            # An element at position `j` in the current row is the 
+            # sum of elements at position `j` and `j-1` in the previous row.
+            next_number = previous_row[j] + previous_row[j - 1]
+            
+            # Append the new element to the current_row
+            current_row.append(next_number)
+            
+        current_row.append(1) # append the default last element
+    return current_row
