8ec0cce357394c417ab15ad5aeb219daeebd9651
horpeazy <iyamuhope.nosa647@gmail.com>
2022-10-08 16:23:53 +0200

    feat: skip i delete j

--- data structures/singly_linked_list.py
+++ data structures/singly_linked_list.py
@@ -276,12 +276,13 @@ class SortedLinkedList:
 
         return None
 
-"""
- Given a linked list with integer data, arrange the elements in such a manner that
- all nodes with even numbers are placed after odd numbers. Do not create any new 
- nodes and avoid using any other data structure. The relative order of even and 
- odd elements must not change.
-"""
+
+# ------------------------------------------------------------------------------ #
+# Given a linked list wth integer data,arrange the elements in such a manner that#
+# all nodes with even numbers are placed after odd numbers. Do not create any new#
+# nodes and avoid using any other data structure. The relative order of even and #
+# odd elements must not change.                                                  #
+# -------------------------------------------------------------------------------#
 
 """
 parameter: - head of the given linked list
@@ -350,6 +351,68 @@ def even_after_odd(head):
     odd_tail.next = even_head       # <-- Append the EVEN sub-list to the tail of ODD sub-list
     
     return odd_head
+
+# ------------------------------------------------------------------------------ #
+# You are given the head of a linked list and two integers, i and j. You have to #
+# retain the first i nodes and then delete the next j nodes. Continue doing so   #
+# until the end of the linked list.                                              #
+# -------------------------------------------------------------------------------#
+
+# Solution
+"""
+:param: head - head of linked list
+:param: i - first `i` nodes that are to be skipped
+:param: j - next `j` nodes that are to be deleted
+return - return the updated head of the linked list
+"""
+'''
+The Idea: 
+Traverse the Linkedist. Make use of two references - `current` and `previous`.
+ - Skip `i-1` nodes. Keep incrementing the `current`. Mark the `i-1`^th node as `previous`. 
+ - Delete next `j` nodes. Keep incrementing the `current`.
+ - Connect the `previous.next` to the `current`
+'''
+def skip_i_delete_j(head, i, j):
+    # Edge case - Skip 0 nodes (means Delete all nodes)
+    if i == 0:
+        return None
+    
+    # Edge case - Delete 0 nodes
+    if j == 0:
+        return head
+    
+    # Invalid input
+    if head is None or j < 0 or i < 0:
+        return head
+
+    # Helper references
+    current = head
+    previous = None
+    
+    # Traverse - Loop untill there are Nodes available in the LinkedList
+    while current:
+        
+        '''skip (i - 1) nodes'''
+        for _ in range(i - 1):
+            if current is None:
+                return head
+            current = current.next
+        previous = current
+        current = current.next
+        
+        '''delete next j nodes'''
+        for _ in range(j):
+            if current is None:
+                break
+            next_node = current.next
+            current = next_node
+        
+        '''Connect the `previous.next` to the `current`''' 
+        previous.next = current
+    
+    # Loop ends
+    
+    return head
 
 def sort(array):
     """
