f6869d6cf5ed415dc5e2cbd9c6c7f7e9cbecac96
horpeazy <iyamuhope.nosa647@gmail.com>
2022-10-25 03:05:04 +0200

    feat: bubble sort

---
+++ algorithms/bubble_sort.py
@@ -0,0 +1,23 @@
+def bubble_sort_1(l):
+    for iteration in range(len(l)):
+        for index in range(1, len(l)):
+            this = l[index]
+            prev = l[index - 1]
+
+            if prev <= this:
+                continue
+
+            l[index] = prev
+            l[index - 1] = this
+
+def bubble_sort_2(l):
+    for iteration in range(len(l)):
+        for index in range(1, len(l)):
+            this_hour, this_min = l[index]
+            prev_hour, prev_min = l[index - 1]
+
+            if prev_hour > this_hour or (prev_hour == this_hour and prev_min > this_min):
+                continue
+
+            l[index] = (prev_hour, prev_min)
+            l[index - 1] = (this_hour, this_min)
