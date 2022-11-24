e41765d3f6faaa2c26db48265b8da5bf0b92a437
horpeazy <iyamuhope.nosa647@gmail.com>
2022-11-24 03:50:14 +0100

    feat: shortest path alogothm

--- algorithms/shortest_path_algorithm.py
+++ algorithms/shortest_path_algorithm.py
@@ -6,8 +6,16 @@ def calc_dist(start, end):
     return dist
 
 def shortest_path(M,start,goal):
-    print("shortest path called")
-    dist_map = {start: 0}
+    """"
+    Calculate the shortest path between two points on a map
+    
+    Args:
+    M(map object) - map
+    start(int) - start position
+    goal(int) - end position
+    
+    """
+    frontiers = {start: 0}
     goal_pos = M.intersections[goal]
     prev_map = {}
     path = [goal]
@@ -18,29 +26,29 @@ def shortest_path(M,start,goal):
         return
     
     while current != goal:
-        curr_dist = dist_map[current]
+        curr_dist = frontiers[current]
         curr_pos = M.intersections[current]
 
         for intersection in M.roads[current]:             #add intersections and corresponding dist ti dist_map
             if intersection in visited:
                 continue
-            if intersection in dist_map:
+            if intersection in frontiers:
                 pos = M.intersections[intersection]           # coordinates
                 dist = calc_dist(pos, curr_pos) + curr_dist
-                if dist < dist_map[intersection]:
-                    dist_map[intersection] = dist
+                if dist < frontiers[intersection]:
+                    frontiers[intersection] = dist
                     prev_map[intersection] = current
                     continue
             else:
                 pos = M.intersections[intersection]           # coordinates
                 dist = calc_dist(pos, curr_pos) + curr_dist
-                dist_map[intersection] = dist
+                frontiers[intersection] = dist
                 prev_map[intersection] = current              # store prev intersection to aid tracing path
-        dist_map.pop(current)                             # remove visted intersection from dist_map
+        frontiers.pop(current)                             # remove visted intersection from dist_map
         visited.add(current)
 
         # get intersection with shortest path using (f + g)
-        current = min(dist_map.keys(), key=lambda x: dist_map[x] + calc_dist(M.intersections[x], goal_pos))
+        current = min(frontiers.keys(), key=lambda x: frontiers[x] + calc_dist(M.intersections[x], goal_pos))
 
     # retrieve the path
     while current != start:
